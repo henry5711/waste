50f8c7daa8f024aadd12fe3312998c6961cc6391
Marcos L <marcoslopez1895@gmail.com>
2022-02-10 16:04:29 -0400

    arreglando show de suscripciones

--- app/Repositories/suscripciones/suscripcionesRepository.php
+++ app/Repositories/suscripciones/suscripcionesRepository.php
@@ -47,7 +47,7 @@ class suscripcionesRepository extends CrudRepository
     public function _show($id)
     {
         $suscripcion = suscripciones::with(['Clientes','Productos'])->where('id',$id)->get();
-        $suscripcion->ico = env('APP_URL').$suscripcion->ico;
+        $suscripcion[0]['ico'] = env('APP_URL').$suscripcion[0]->ico;
         return $suscripcion;
     }
 
