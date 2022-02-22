bdd9e5977d3009d785eb08138b6c842f40ebe068
henry <henryrom57@gmail.com>
2022-02-22 10:39:57 -0400

    cambiecito visual

--- app/Http/Controllers/rutas/rutasController.php
+++ app/Http/Controllers/rutas/rutasController.php
@@ -111,8 +111,8 @@ class rutasController extends CrudController
       $archivo->getActiveSheet()->getColumnDimension('E')->setWidth(220, 'px');
       $archivo->getActiveSheet()->getColumnDimension('F')->setWidth(220, 'px');
       $archivo->getActiveSheet()->getColumnDimension('G')->setWidth(220, 'px');
-      $archivo->getActiveSheet()->getColumnDimension('H')->setWidth(220, 'px');
-      $archivo->getActiveSheet()->getColumnDimension('I')->setWidth(220, 'px');
+      $archivo->getActiveSheet()->getColumnDimension('H')->setWidth(300, 'px');
+      $archivo->getActiveSheet()->getColumnDimension('I')->setWidth(300, 'px');
       $archivo->getActiveSheet()->getColumnDimension('J')->setWidth(220, 'px');
 
         //AQUI CENTRO LOS TITULOS
@@ -154,6 +154,7 @@ class rutasController extends CrudController
             $hoja->setCellValue('H'.$fila,$key->pt_ini);
             $hoja->setCellValue('I'.$fila,$key->pt_fin);
             $hoja->setCellValue('J'.$fila,$key->status);
+            $fila++;
         }
 
         
