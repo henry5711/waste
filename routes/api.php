dcd04f362f7014bf9b42a4f4a44f80d6099cf669
henry <henryrom57@gmail.com>
2022-02-25 08:55:27 -0400

    nuevo endpoint

--- routes/api.php
+++ routes/api.php
@@ -90,6 +90,7 @@ $router->group(['prefix' => 'api'], function (Router $router) {
     /** routes para operation **/ 
  
     $router->get('operations/filter/range', 'operation\operationController@filtro');
+    $router->get('sucursales/total', 'operation\operationController@sucurconsulta');
     $router->get('operations', 'operation\operationController@_index');
     $router->get('operations/creadas', 'operation\operationController@icreadas');
     $router->get('operations/filter/report', 'operation\operationController@reportope');
